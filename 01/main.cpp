8c7196152c909056c207a5a7f8710004ce96f71a
Remus Schnell <schnell_remus@yahoo.com>
2023-12-02 00:27:36 +0100

    Day 01: second star

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 
+#include <vector>
+
 int main(int argc, char *argv[]) {
 
     std::string inputFilePath;
@@ -18,16 +20,51 @@ int main(int argc, char *argv[]) {
     }
 
     const std::string digits = "0123456789";
+    const std::vector<std::string> numbers = {
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    };
+
+
+    std::vector<std::string> reversedNumbers;
+    for (auto& nb:numbers) {
+        std::string revNb = std::string(nb.rbegin(), nb.rend());
+        reversedNumbers.push_back(std::move(revNb));
+    }
+
+    auto getFirstDigit = [&digits](const std::string& line, const std::vector<std::string>& numbers) {
+        const size_t firstDigitPos = line.find_first_of(digits);
+        size_t firstDigit = 0;
+        if (firstDigitPos != std::string::npos) {
+            firstDigit = line[firstDigitPos] - '0';
+        }
+        size_t firstPos = firstDigitPos;
+        for(size_t i=0; i<numbers.size(); ++i) {
+            const size_t pos = line.find(numbers[i]);
+            if (pos < firstPos) {
+                firstPos = pos;
+                firstDigit = i+1;
+            }
+        }
+        return firstDigit;
+    };
 
     std::string line;
     size_t sum = 0;
     while(getline(listFile, line)) {
         if (!line.empty()) {
-            const size_t firstDigitPos = line.find_first_of(digits);
-            const size_t lastDigitPos = line.find_last_of(digits);
-            if ((firstDigitPos != std::string::npos) && (lastDigitPos != std::string::npos)) {
-                sum += 10 * static_cast<size_t>(line[firstDigitPos] - '0') + static_cast<size_t>(line[lastDigitPos] - '0');
-            }
+            const size_t firstDigit = getFirstDigit(line, numbers);
+
+            std::string reversedLine = std::string(line.rbegin(), line.rend());
+            const size_t lastDigit = getFirstDigit(reversedLine, reversedNumbers);
+            sum += 10 * firstDigit + lastDigit;
         }
     }
 
