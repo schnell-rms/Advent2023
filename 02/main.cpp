a3bb2796e8bc90f6facc61299e8f06e37eaa5465
Remus Schnell <schnell_remus@yahoo.com>
2023-12-02 10:56:40 +0100

    Day 02: second star

--- 02/main.cpp
+++ 02/main.cpp
@@ -58,22 +58,62 @@ int main(int argc, char *argv[]) {
         return true;
     };
 
+    // Could overwrite or merge with the above lambda. Whatever, write a new one:
+    auto getMinGamePower = [&](const std::string &game) {
+        std::string grab;
+        std::istringstream gameStream(game);
+        int maxRed(0), maxBlue(0), maxGreen(0);
+        while (std::getline(gameStream, grab, ';'))
+        {
+            int red = 0, blue = 0, green = 0;
+            smatch sm;
+            if (regex_search(grab, sm, regex("(\\d+) blue"))) {
+                blue = std::stoi(sm[1]);
+                if (blue > maxBlue) {
+                    maxBlue = blue;
+                }
+            }
+
+            if (regex_search(grab, sm, regex("(\\d+) red"))) {
+                red = std::stoi(sm[1]);
+                if (red > maxRed) {
+                    maxRed = red;
+                }
+            }
+
+            if (regex_search(grab, sm, regex("(\\d+) green"))) {
+                green = std::stoi(sm[1]);
+                if (green > maxGreen) {
+                    maxGreen = green;
+                }
+            }
+        }
+
+        return maxRed * maxBlue * maxGreen;
+    };
+
     std::string line;
     int gameId = 1;
     int gameSum = 0;
+    int gamePowerSum = 0;
     while(getline(listFile, line)) {
         if (!line.empty()) {
             size_t startPos = 0;//line.find(':');
             // Could extract gameId using (regex_search(line, sm, regex("Game (\\d+)"))) 
             // Rely on counting lines, instead:
+
+            // First star:
             if (checkGame(line.substr(startPos + 1))) {
                 gameSum += gameId;
             }
 
+            // Second star:
+            gamePowerSum += getMinGamePower(line.substr(startPos + 1));
             gameId++;
         }
     }
 
     cout << "Sum of possible games: " << gameSum << endl;
+    cout << "Sum of minimum game powers: " << gamePowerSum << endl;
     return EXIT_SUCCESS;
 }
