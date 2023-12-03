fea96e0cdfd8e0480215e38b281302c71df72034
Remus Schnell <schnell_remus@yahoo.com>
2023-12-03 11:39:08 +0100

    Day 03: second star

--- 03/main.cpp
+++ 03/main.cpp
@@ -5,7 +5,28 @@
 #include <regex>
 #include <string>
 
+#include <vector>
+#include <unordered_map>
+
 using namespace std;
+
+struct SGear {
+    void addNumber(int number)  {
+        _numbers.push_back(number);
+    }
+    bool isGear() const { return _numbers.size() == 2; }
+    int ratio() {
+        int ret = 1;
+        for (int n:_numbers) {
+            ret *= n;
+        }
+        return ret;
+    }
+    private:
+        std::vector<int> _numbers;
+};
+
+using TGears = unordered_map<uint64_t, SGear>;
 
 int main(int argc, char *argv[]) {
 
@@ -23,11 +44,17 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
     }
 
-    auto hasSymbol = [&](const std::string& line, size_t startPos, size_t endPos) {
+    auto hasSymbol = [&](const std::string& line,
+                         size_t startPos,
+                         size_t endPos,
+                         size_t lineIndex,
+                         TGears& gears,
+                         int number) {
         if (line.empty()) {
             return false;
         }
         
+        bool isConnected = false;
         for (size_t i = ((startPos > 0) ? startPos - 1 : startPos);
              i < ((endPos < line.size()) ? endPos + 1 : line.size());
              ++i)
@@ -35,13 +62,22 @@ int main(int argc, char *argv[]) {
             if (((line[i] < '0') || (line[i]>'9')) && // not a number
                  (line[i] != '.')) // and not a point
             {
-                return true;
+                isConnected = true;
+                if (line[i] == '*') // a potential gear
+                {
+                    gears[(lineIndex << 32) | i].addNumber(number);
+                }
             }
         }
-        return false;
+        return isConnected;
     };
 
-    auto getLineSum = [&](const std::string& prevLine, const std::string& currLine, const std::string& nextLine) {
+    auto getLineSum = [&](  const std::string& prevLine,
+                            const std::string& currLine,
+                            const std::string& nextLine,
+                            size_t lineIndex,
+                            TGears& potentialGears)
+    {
             const char kPointChar = '.';
             int lineSum = 0;
             // Get the numbers from the current line           
@@ -49,31 +85,33 @@ int main(int argc, char *argv[]) {
             string::const_iterator searchStart( currLine.cbegin() );
             while (regex_search(searchStart, currLine.cend(), sm, regex("(\\d+)"))) {
                 // Validate the match:
+                int number = std::stoi(sm[1]);
                 auto posStart = (searchStart - currLine.begin()) + sm.position(1);
                 auto posEnd = posStart + sm.str(1).size();
 
                 bool isConnected = false;
-                if ((posStart > 0) and (currLine[posStart - 1] != kPointChar)) {
-                    isConnected = true;
-                }
+                // For the second star, use hasSymbol() instead
+                // if ((posStart > 0) and (currLine[posStart - 1] != kPointChar)) {
+                //     isConnected = true;
+                // }
 
-                if ((posEnd < currLine.size()) and (currLine[posEnd] != kPointChar)) {
-                    isConnected = true;
-                }
+                // if ((posEnd < currLine.size()) and (currLine[posEnd] != kPointChar)) {
+                //     isConnected = true;
+                // }
 
-                if (!isConnected) {
-                    isConnected =   hasSymbol(prevLine, posStart, posEnd) ||
-                                    hasSymbol(nextLine, posStart, posEnd);
-                }
+                // Have to run all hasSymbol, for the second star:
+                isConnected = hasSymbol(prevLine, posStart, posEnd, lineIndex - 1, potentialGears, number) || isConnected;
+                isConnected = hasSymbol(currLine, posStart, posEnd, lineIndex    , potentialGears, number) || isConnected;
+                isConnected = hasSymbol(nextLine, posStart, posEnd, lineIndex + 1, potentialGears, number) || isConnected;
+
+                // TODO: could optimize memory usage by storing every moment only the potential Gears from 2 lines
 
                 if (isConnected) {
-                    cout << sm[1] << endl;
-                    lineSum += std::stoi(sm[1]);
+                    lineSum += number;
                 }
                 
                 searchStart = sm.suffix().first;
             }
-            cout << "--------------" << endl;
 
             return lineSum;
     };
@@ -83,17 +121,30 @@ int main(int argc, char *argv[]) {
     std::string currLine;
     std::string nextLine;
     getline(listFile, currLine);
+    size_t lineIndex = 0;
+    TGears potentialGears;
     while(getline(listFile, nextLine)) {
         if (!nextLine.empty()) {
 
-            totalSum += getLineSum(prevLine, currLine, nextLine);
+            totalSum += getLineSum(prevLine, currLine, nextLine, lineIndex, potentialGears);
         }
 
         prevLine = std::move(currLine);
         currLine = std::move(nextLine);
+        lineIndex++;
     }
 
-    totalSum += getLineSum(prevLine, currLine, nextLine);
+    totalSum += getLineSum(prevLine, currLine, nextLine, lineIndex, potentialGears);
     cout << "Total sum: " << totalSum << endl;
+
+    // Filter gears:
+    int gearsSum = 0;
+    for (auto it:potentialGears) {
+        if (it.second.isGear()) {
+            gearsSum += it.second.ratio();
+        }
+    }
+
+    cout << "Gear sum: " << gearsSum << endl;
     return EXIT_SUCCESS;
 }
