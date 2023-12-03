c32ec2baafce9051d8cac0e9d612fc41fb97e5f1
Remus Schnell <schnell_remus@yahoo.com>
2023-12-03 12:34:39 +0100

    Day 03: second star - optimizations

--- 03/main.cpp
+++ 03/main.cpp
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <unordered_map>
+#include <tuple>
 
 using namespace std;
 
@@ -26,7 +27,30 @@ struct SGear {
         std::vector<int> _numbers;
 };
 
-using TGears = unordered_map<uint64_t, SGear>;
+using TGears = unordered_map<size_t, SGear>;
+
+struct S3LinesGears {
+    TGears previous;
+    TGears current;
+    TGears next;
+    void goForward() {
+        previous = std::move(current);
+        current  = std::move(next);
+    }
+};
+
+int realGearsSum(TGears gears) {
+    int gearsSum = 0;
+    for (auto it:gears) {
+        if (it.second.isGear()) {
+            gearsSum += it.second.ratio();
+        }
+    }
+    return gearsSum;
+}
+
+const char kPointChar = '.';
+const char kGearChar = '*';
 
 int main(int argc, char *argv[]) {
 
@@ -60,12 +84,12 @@ int main(int argc, char *argv[]) {
              ++i)
         {
             if (((line[i] < '0') || (line[i]>'9')) && // not a number
-                 (line[i] != '.')) // and not a point
+                 (line[i] != kPointChar)) // and not a point
             {
                 isConnected = true;
-                if (line[i] == '*') // a potential gear
-                {
-                    gears[(lineIndex << 32) | i].addNumber(number);
+                if (line[i] == kGearChar) {
+                    // a potential gear
+                    gears[i].addNumber(number);
                 }
             }
         }
@@ -76,9 +100,8 @@ int main(int argc, char *argv[]) {
                             const std::string& currLine,
                             const std::string& nextLine,
                             size_t lineIndex,
-                            TGears& potentialGears)
+                            S3LinesGears& gears)
     {
-            const char kPointChar = '.';
             int lineSum = 0;
             // Get the numbers from the current line           
             smatch sm;
@@ -90,21 +113,24 @@ int main(int argc, char *argv[]) {
                 auto posEnd = posStart + sm.str(1).size();
 
                 bool isConnected = false;
-                // For the second star, use hasSymbol() instead
-                // if ((posStart > 0) and (currLine[posStart - 1] != kPointChar)) {
-                //     isConnected = true;
-                // }
+                // For the current line already look at, and only at, the neighbouring chars:
+                if ((posStart > 0) and (currLine[posStart - 1] != kPointChar)) {
+                    if (currLine[posStart - 1] == kGearChar) {
+                        gears.current[posStart-1].addNumber(number);
+                    }
+                    isConnected = true;
+                }
 
-                // if ((posEnd < currLine.size()) and (currLine[posEnd] != kPointChar)) {
-                //     isConnected = true;
-                // }
+                if ((posEnd < currLine.size()) and (currLine[posEnd] != kPointChar)) {
+                    if (currLine[posEnd] == kGearChar) {
+                        gears.current[posEnd].addNumber(number);
+                    }
+                    isConnected = true;
+                }
 
                 // Have to run all hasSymbol, for the second star:
-                isConnected = hasSymbol(prevLine, posStart, posEnd, lineIndex - 1, potentialGears, number) || isConnected;
-                isConnected = hasSymbol(currLine, posStart, posEnd, lineIndex    , potentialGears, number) || isConnected;
-                isConnected = hasSymbol(nextLine, posStart, posEnd, lineIndex + 1, potentialGears, number) || isConnected;
-
-                // TODO: could optimize memory usage by storing every moment only the potential Gears from 2 lines
+                isConnected = hasSymbol(prevLine, posStart, posEnd, lineIndex - 1, gears.previous, number) || isConnected;
+                isConnected = hasSymbol(nextLine, posStart, posEnd, lineIndex + 1, gears.next, number) || isConnected;
 
                 if (isConnected) {
                     lineSum += number;
@@ -113,20 +139,29 @@ int main(int argc, char *argv[]) {
                 searchStart = sm.suffix().first;
             }
 
-            return lineSum;
+            const int gearsSum = realGearsSum(gears.previous);
+            gears.goForward();
+
+            return std::make_tuple(lineSum, gearsSum);
     };
     
     int totalSum = 0;
+    int totalGearSum = 0;
     std::string prevLine;
     std::string currLine;
     std::string nextLine;
+    S3LinesGears gears;
+
     getline(listFile, currLine);
     size_t lineIndex = 0;
-    TGears potentialGears;
     while(getline(listFile, nextLine)) {
         if (!nextLine.empty()) {
 
-            totalSum += getLineSum(prevLine, currLine, nextLine, lineIndex, potentialGears);
+            auto [partsSum, gearSum] = getLineSum(prevLine, currLine, nextLine,
+                                                  lineIndex,
+                                                  gears);
+            totalSum += partsSum;
+            totalGearSum += gearSum;
         }
 
         prevLine = std::move(currLine);
@@ -134,17 +169,11 @@ int main(int argc, char *argv[]) {
         lineIndex++;
     }
 
-    totalSum += getLineSum(prevLine, currLine, nextLine, lineIndex, potentialGears);
+    auto [partsSum, gearSum] = getLineSum(prevLine, currLine, nextLine, lineIndex, gears);
+    totalSum += partsSum;
+    totalGearSum += gearSum;
+
     cout << "Total sum: " << totalSum << endl;
-
-    // Filter gears:
-    int gearsSum = 0;
-    for (auto it:potentialGears) {
-        if (it.second.isGear()) {
-            gearsSum += it.second.ratio();
-        }
-    }
-
-    cout << "Gear sum: " << gearsSum << endl;
+    cout << "Gear sum: " << totalGearSum << endl;
     return EXIT_SUCCESS;
 }
