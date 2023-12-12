d7df61dd88f0f7157a531ff8e94bdec7e11dba58
Remus Schnell <schnell_remus@yahoo.com>
2023-12-12 13:33:34 +0100

    Day 12: clean code, add nice comments

--- 12/main.cpp
+++ 12/main.cpp
@@ -2,8 +2,9 @@
 #include <sstream>
 #include <fstream>
 
-#include <regex>
+#include <optional>
 #include <string>
+#include <unordered_map>
 
 #include <utils.h>
 
@@ -12,46 +13,84 @@ using namespace std;
 using TMap = std::vector<std::string>;
 using TArrangement = std::vector<std::vector<long>>;
 
-bool isMatch(const std::string& str, const std::vector<long> &arr) {
-        assert(std::string::npos == str.find('?'));
-        size_t i=0;
-        size_t ssz = str.size();
-        size_t asz = arr.size();
-        size_t j = 0;
-        while ((i<ssz) && (j<asz)) {
-            while ((i<ssz) && (str[i]=='.')) ++i;
-            long count = 0;
-            while ((i<ssz) && (str[i]=='#')) {
-                count++;
-                ++i;
-            }
-            if (count != arr[j]) {
-                return false;
-            }
+using TCache = std::unordered_map<size_t, size_t>;
 
-            j++;
-        }
-        if (j!=asz) return false;
-        if (std::string::npos == str.find('#',i)) {
-            return true;
-        }
-        return false;
+size_t hashSituation(size_t strIdx, size_t arrIdx, std::optional<size_t> mandatoryNb) {
+    size_t ret = strIdx << 32;
+    ret |= arrIdx << 16;
+    ret |= (mandatoryNb.has_value() ? 1 : 0) << 15;
+    ret |= (mandatoryNb.has_value() ? mandatoryNb.value() : 0);
+    return ret;
+}
+
+size_t getNb(std::string str, size_t idx, std::vector<long> arr, size_t arrIdx,  std::optional<size_t> mandatoryNb, TCache& cache) {
+
+    auto needsSpringNow = [&mandatoryNb]() {
+        return mandatoryNb && (mandatoryNb.value() > 0);
     };
 
-size_t getNb(std::string str, const std::vector<long> &arr) {
-    size_t pos = str.find('?');
-    size_t n=0;
-    if (pos != std::string::npos) {
-        str[pos] = '.';
-        n = getNb(str, arr);
-        str[pos] = '#';
-        n += getNb(str, arr);
-    } else if (isMatch(str, arr)) {
-        return 1UL;
+    auto needsPointNow = [&mandatoryNb]() {
+        return mandatoryNb && (mandatoryNb.value() == 0);
+    };
+
+    if (str.size() == idx) {
+        // All springs are counted. Need to count more?
+        return (needsSpringNow() || (arrIdx < arr.size())) ? 0 : 1;
+    }
+
+    if ((arrIdx == arr.size()) && !needsSpringNow()) {
+        // Nothing else to match, check whether there are still uncounted springs:
+        return std::string::npos == str.find('#', idx) ? 1 : 0;
+    }
+
+    const size_t key = hashSituation(idx, arrIdx, mandatoryNb);
+    // Inspect the cache:
+    auto it = cache.find(key);
+    if (it != cache.end()) {
+        return it->second;
+    }
+
+    size_t nb;
+    switch (str[idx]) {
+        case '.':
+            nb = needsSpringNow()
+                    ? 0
+                    : getNb(str, idx + 1, arr, arrIdx, std::optional<size_t>{}, cache);
+            break;
+        case '#':// Spring found:
+            if (needsSpringNow()) {
+                mandatoryNb = mandatoryNb.value() - 1;//count it
+                nb = getNb(str, idx + 1, arr, arrIdx, mandatoryNb, cache);
+            } else if (needsPointNow()) {
+                nb = 0;
+            } else {
+                // Take this spring in the next group and retry:
+                mandatoryNb = arr[arrIdx];// Start counting and run again:
+                nb = getNb(str, idx, arr, arrIdx + 1, mandatoryNb, cache);
+            }
+            break;
+        case '?':
+            if (needsSpringNow()) { // Force it to spring:
+                mandatoryNb = mandatoryNb.value() - 1;
+                nb = getNb(str, idx + 1, arr, arrIdx, mandatoryNb, cache);
+            } else if (needsPointNow()) { // FOrce it to point:
+                nb = getNb(str, idx + 1, arr, arrIdx, std::optional<size_t>{}, cache);
+            } else { // Try both:
+                // Jump over as being a '.'
+                const size_t nPoint = getNb(str, idx + 1, arr, arrIdx, std::optional<size_t>{}, cache);
+                // Or retry, with mandatory # this time:
+                mandatoryNb = arr[arrIdx];// start counter: next call will force it to spring:
+                const size_t nSpring = getNb(str, idx, arr, arrIdx + 1, mandatoryNb, cache);
+                nb = nPoint + nSpring;
+            }
+            break;
+        default:
+            assert(false);
     }
     
-    return n;
-};
+    cache[key] = nb;
+    return nb;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -68,14 +107,7 @@ int main(int argc, char *argv[]) {
         std::cout << "Could not open input file" << std::endl;
         return EXIT_FAILURE;
     }
-    
-    
-    auto is = isMatch("..#..#....###.", {1,1,3});
-    cout << (is ? "DA" : "NU") << endl;
-    
-    auto n = getNb(".??..??...?##.", {1,1,3});
-    cout << "AAAA " << n << endl;
-    
+
     TMap springMap;
     TArrangement arrangement;
 
@@ -85,18 +117,45 @@ int main(int argc, char *argv[]) {
             size_t pos = line.find(' ');
             springMap.emplace_back(line.substr(0,pos));
             arrangement.push_back(allNumbers(line));
-            // cout << springMap.back() << endl;
-            // printVector(arrangement.back());
         }
     }
 
+    auto unfold = [&]() {
+        for (auto &str:springMap) {
+            std::string unfoldedStr(str);
+            for (int i=1; i<5;  i++) {
+                unfoldedStr += '?';
+                unfoldedStr += str;
+            }
+            str.swap(unfoldedStr);
+        }
+        for (auto &arr:arrangement) {
+            auto newArr = arr;
+            for (int i=0; i<4;  i++) {
+                newArr.insert(std::end(newArr), std::begin(arr), std::end(arr));
+            }
+            arr.swap(newArr);
+        }
+    };
+
     size_t totalSum = 0;
     for (size_t i = 0; i< springMap.size(); ++i) {
-        auto lineNb = getNb(springMap[i], arrangement[i]);
+        TCache cache;
+        const auto lineNb = getNb(springMap[i], 0UL, arrangement[i], 0UL, std::optional<size_t>(), cache);
         totalSum += lineNb;
     }
 
-    cout << "Total sum: " << totalSum;
+    cout << "Silver sum: " << totalSum << endl;
+
+    unfold();
+
+    totalSum = 0;
+    for (size_t i = 0; i< springMap.size(); ++i) {
+        TCache cache;
+        const auto lineNb = getNb(springMap[i], 0UL, arrangement[i], 0UL, std::optional<size_t>(), cache);
+        totalSum += lineNb;
+    }
+    cout << "Golder sum: " << totalSum << endl;
 
     return EXIT_SUCCESS;
 }
