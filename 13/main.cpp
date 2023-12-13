95192fade98079c9d51e09bb38384c4b9bc04f1e
Remus Schnell <schnell_remus@yahoo.com>
2023-12-13 07:51:11 +0100

    Day 13: second star

--- 13/main.cpp
+++ 13/main.cpp
@@ -18,13 +18,52 @@ size_t checkReflection(TMap &numbers) {
         if (numbers[i] == numbers[i-1]) {
             const size_t reflectionSize = std::min(i,sz - i);
             bool isReflection = true;
-            for (size_t j=0; j<reflectionSize; j++) {
+            for (size_t j=1; j<reflectionSize; j++) {
                 if (numbers[i+j] != numbers[i - j - 1]) {
                     isReflection = false;
                     break;
                 }
             }
             if (isReflection) {
+                return i;
+            }
+        }
+    }
+    return ret;
+}
+
+bool isPowerOf2(uint64_t x) {
+    // aka has at most one 1 bit
+    return (x & (x-1)) == 0;
+}
+
+bool equalSmudgeNumbers(uint64_t x, uint64_t y, size_t &smudgeCount) {
+    if (x == y) 
+        return true;
+    const uint64_t eval = x^y;
+    const bool itIs = isPowerOf2(eval);
+    if (itIs) {
+        smudgeCount++;
+    }
+    return itIs;
+}
+
+size_t checkReflectionSmudge(TMap &numbers) {
+    size_t ret = 0;
+    const size_t sz = numbers.size();
+    for (size_t i = 1; i<sz; ++i) {
+        size_t smudgeCount = 0;
+        if (equalSmudgeNumbers(numbers[i], numbers[i-1], smudgeCount)) {
+            const size_t reflectionSize = std::min(i,sz - i);
+            bool isReflection = true;
+            for (size_t j=1; j<reflectionSize; j++) {
+                if (    !equalSmudgeNumbers(numbers[i+j],numbers[i - j - 1], smudgeCount) 
+                    ||  (smudgeCount > 1)) {
+                    isReflection = false;
+                    break;
+                }
+            }
+            if (isReflection && (smudgeCount == 1)) {
                 return i;
             }
         }
@@ -79,20 +118,30 @@ int main(int argc, char *argv[]) {
         }
     } while(getline(listFile, line));
 
-    printMatrix(lineNumbers);
-    printMatrix(columnNumbers);
+    if (isDebug) {
+        printMatrix(lineNumbers);
+        printMatrix(columnNumbers);
+    }
 
     size_t nbVerticalSymetries = 0;
     size_t nbHorizontalSymetries = 0;
+
+    size_t nbVerticalSymetries_smudge = 0;
+    size_t nbHorizontalSymetries_smudge = 0;
 
     const size_t sz = lineNumbers.size();
     for (size_t i=0; i<sz; ++i) {
         nbVerticalSymetries += checkReflection(columnNumbers[i]);
         nbHorizontalSymetries += checkReflection(lineNumbers[i]);
+
+        nbVerticalSymetries_smudge += checkReflectionSmudge(columnNumbers[i]);
+        nbHorizontalSymetries_smudge += checkReflectionSmudge(lineNumbers[i]);
     }
 
     const size_t result = nbVerticalSymetries + 100 * nbHorizontalSymetries;
     cout << "Result is: " << result << endl;
 
+    const size_t result_smudge = nbVerticalSymetries_smudge + 100 * nbHorizontalSymetries_smudge;
+    cout << "Smudge result is: " << result_smudge << endl;
     return EXIT_SUCCESS;
 }
