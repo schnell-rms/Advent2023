dada7714cd349f1a1601b2432b1f930f0d6dfd51
Remus Schnell <schnell_remus@yahoo.com>
2023-12-14 08:13:03 +0100

    Day 14: second star

--- 14/main.cpp
+++ 14/main.cpp
@@ -37,9 +37,8 @@ int main(int argc, char *argv[]) {
     const size_t kNbLines = lines.size();
     const size_t kNbColumns = lines[0].size();
     std::vector<long> factors(kNbColumns, kNbLines);
-    printVector(lines);
+    // printMatrix(lines);
     for (size_t i=0; i<kNbLines; ++i) {
-
         for (size_t j=0; j<kNbColumns; ++j) {
             if (lines[i][j] == '#') {
                 factors[j] = kNbLines - i - 1;// - 1: calculate for the next line
@@ -50,7 +49,74 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    cout << "Total: " << total << endl;
+    cout << "Total first star: " << total << endl;
+
+    auto getWeigth = [&]() {
+        size_t northWeigth = 0;
+        for (size_t i=0; i<kNbLines; ++i) {
+            for (size_t j=0; j<kNbColumns; ++j) {
+                if (lines[i][j] == 'O') {
+                    northWeigth += kNbColumns - i;
+                }
+            }
+        }
+        return northWeigth;
+    };
+
+
+    auto moveWest = [&]() {
+        for (size_t i=0; i<kNbLines; ++i) {
+            size_t squarePos = -1;//overflow: ok!
+            for (size_t j=0; j<kNbColumns; ++j) {
+                if (lines[i][j] == '#') {
+                    squarePos = j;
+                } else if (lines[i][j] == 'O') {
+                    lines[i][j] = '.';
+                    lines[i][squarePos + 1] = 'O';//maybe put 0 back...
+                    squarePos++;
+                }
+            }
+        }
+    };
+
+    auto clockwiseRotation = [&]() {
+        std::vector<string> rotated(kNbColumns);
+        for (size_t j=0; j<kNbColumns; ++j) {
+            rotated[j].reserve(kNbLines);
+            for (size_t i=1; i<=kNbLines; ++i) {
+                rotated[j] += lines[kNbLines - i][j];
+            }
+        }
+        lines.swap(rotated);
+    };
+
+    // printMatrix(lines);
+    // clockwiseRotation();
+    // printMatrix(lines);
+
+    // Face the north to the west:
+    for (size_t i=0; i<3; i++) {
+        clockwiseRotation();
+    }
+
+    // printMatrix(lines);
+
+    // Try with 1000, why not...
+    // P.S. it worked... So at some point it becomes stable
+    // Meaning states could somehow be cached... just in case 1000 is like 1 billion
+    // And it is not: TODO: caching!
+    for (size_t i=0; i<1000; i++) {
+        for (size_t j=0; j<4; j++) {
+            moveWest();
+            clockwiseRotation();
+        }
+        // printMatrix(lines);
+    }
+
+    // Rotate North to north:
+    clockwiseRotation();
+    // printMatrix(lines);
+    cout << "Total second star: " << getWeigth() << endl;
 
     return EXIT_SUCCESS;
 }
