b8dcebace17e49906f3f511fe7d0289aedf64b94
Remus Schnell <schnell_remus@yahoo.com>
2023-12-20 19:36:40 +0100

    Day 20: second star with some comments

--- 20/main.cpp
+++ 20/main.cpp
@@ -7,11 +7,14 @@
 #include <vector>
 #include <queue>
 #include <unordered_map>
+#include <numeric>
 
 using namespace std;
 
 const bool gCONJUNTION_DEFAULT_MEMORY = false;
+const bool gFLIP_FLOP_DEFAULT_STATE = false;
 const std::string gBROADCASTER = "broadcaster";
+const std::string gRX_OUTPUT = "rx";
 
 struct SPulse {
     std::string source;
@@ -26,7 +29,7 @@ struct SModule {
     char type = 0;//b, % or &
     std::vector<std::string> targets;
     std::unordered_map<std::string, bool> conjunctionMemory;
-    bool isFlipFlopOn = false;
+    bool isFlipFlopOn = gFLIP_FLOP_DEFAULT_STATE;
 
     void receivePulse(const SPulse &pulse, TQPulses &q) {
         switch (type)
@@ -41,6 +44,20 @@ struct SModule {
             //simply output
             return;
         }
+    }
+
+    bool isConjuntionHigh() const {
+        for (auto it:conjunctionMemory) {
+            if (!it.second) return false;
+        }
+        return true;
+    }
+
+    void clear() {
+        for (auto it:conjunctionMemory) {
+            it.second = gCONJUNTION_DEFAULT_MEMORY;
+        }
+        isFlipFlopOn = gFLIP_FLOP_DEFAULT_STATE;
     }
 
 private:
@@ -61,11 +78,7 @@ private:
     
     void pulseConjunction(const SPulse &p, TQPulses &q) {
         conjunctionMemory[p.source] = p.isHigh;
-        bool allHigh = true;
-        for (auto it:conjunctionMemory) {
-            allHigh = allHigh && it.second;
-        }
-        sendToAll(!allHigh, q);
+        sendToAll(!isConjuntionHigh(), q);
     }
 };
 
@@ -117,27 +130,96 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    TNumber highPulseCounter = 0;
-    TNumber lowPulseCounter = 0;
+    // ________________________________________________________
+    // SILVER
+    // --------------------------------------------------------
+     TNumber highPulseCounter = 0;
+     TNumber lowPulseCounter = 0;
+
+     for (size_t i=0; i< 1000; i++) {
+         TQPulses q;
+         SModule bc = scheme[gBROADCASTER];
+
+         q.push({"", gBROADCASTER, false});
+
+         while (!q.empty()) {
+             SPulse p = q.front();
+             q.pop();
+             highPulseCounter += p.isHigh;
+             lowPulseCounter += !p.isHigh;
+
+             SModule &m = scheme[p.target];
+             m.receivePulse(p, q);
+         }
+     }
+
+     const auto silver = lowPulseCounter * highPulseCounter;
+     cout << "Total silver is: " << silver << endl;
+
+    // ________________________________________________________
+    // GOLD
+    // --------------------------------------------------------
     
-    for (size_t i=0; i< 1000; i++) {
+    // RX has in this example only one input.
+    // On the general case, each input should be treated separatelly and
+    // use the one the sends a low pulse faster.
+    const SModule rx = scheme[gRX_OUTPUT];
+    // So, only one module as input for RX, which is a conjunction module:
+    const SModule &m1 = scheme[rx.conjunctionMemory.begin()->first];
+    // Take its inputs: <name , number of button presses until it gives a high pulse>
+    std::unordered_map<std::string, TNumber> back2Modules;
+    for (const auto &m2:m1.conjunctionMemory) {
+        back2Modules[m2.first] = 0;
+    }
+
+    // Clean everything after silver:
+    for (auto &module:scheme) {
+        module.second.clear();
+    }
+
+    // Used for stopping the loop
+    auto allSet = [&]() {
+        for (const auto &c:back2Modules) {
+            if (c.second == 0) return false;
+        }
+        return true;
+    };
+
+    TNumber n = 0;//number of button presses
+    while (!allSet()) {
+        // Press the button (again)
         TQPulses q;
         SModule bc = scheme[gBROADCASTER];
 
         q.push({"", gBROADCASTER, false});
+        n++;
 
         while (!q.empty()) {
             SPulse p = q.front();
             q.pop();
-            highPulseCounter += p.isHigh;
-            lowPulseCounter += !p.isHigh;
 
             SModule &m = scheme[p.target];
             m.receivePulse(p, q);
+
+            // Did a back2 module sent a high pulse?
+            auto itc2 = back2Modules.find(p.source);
+            if (    (back2Modules.end() != itc2)
+                &&  (itc2->second == 0)
+                &&  p.isHigh)
+            {
+                cout << itc2->first << " ===> " << n - itc2->second << endl;
+                itc2->second = n;
+            }
         }
     }
 
-    const auto silver = lowPulseCounter * highPulseCounter;
-    cout << "Total silver is: " << silver << endl;
+    size_t gold = 1;
+    // Get when all back2 modules will send a high pulse to the back1 module.
+    for (auto &c:back2Modules) {
+        gold = std::lcm(gold,c.second);
+    }
+
+    cout << "Gold is: " << gold << endl;
+
     return EXIT_SUCCESS;
 }
